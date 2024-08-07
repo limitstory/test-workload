cabdc8c0048870cea4bfdf246e48b143a49aaac6
limitstory <teri98@naver.com>
2024-08-07 16:45:34 +0900

    modify code

--- splash2x-ocean_cp/generate.py
+++ splash2x-ocean_cp/generate.py
@@ -5,17 +5,17 @@ job_template = """
 apiVersion: v1
 kind: Pod
 metadata:
-  name: {program_name}-{app_name}{index}
+  name: {program_name}-{app_name2}{index}
   labels:
-    app: {app_name}{index}
+    app: {app_name2}{index}
 spec:
   restartPolicy: OnFailure
   containers:
-  - name: {app_name}{index}
+  - name: {app_name2}{index}
     image: docker.io/limitstory/parsec-modify:latest
     imagePullPolicy: IfNotPresent
     command: ["/home/parsec-3.0/run"]
-    args: ["-a", "run", "-p", "{program_name}.{app_name}", "-i", "native"]
+    args: ["-a", "run", "-p", "{program_name}.{app_name1}", "-i", "native"]
     resources:
       requests:
         memory: {memory_request}Mi
@@ -28,7 +28,8 @@ node_names = ["worker1", "worker2", "worker3"]
 loop_times = 20
 
 program_name = "splash2x"
-app_name = "ocean_cp"
+app_name1 = "ocean_cp"
+app_name2 = "ocean-cp"
 set_memory_limit = 3850
 
 
@@ -37,7 +38,7 @@ memory_percentages = range(10, 21, 1)  # 100%부터 200%까지 10% 단위로 증
 for percentage in memory_percentages:
     memory_request = set_memory_limit // percentage * 10
     memory_limit = set_memory_limit
-    file_name = f'{program_name}-{app_name}-{percentage*10}%.py'
+    file_name = f'{program_name}-{app_name1}-{percentage*10}%.py'
     
     with open(file_name, 'w') as script_file:
         script_file.write("import os\nimport time\n\n")
@@ -45,11 +46,12 @@ for percentage in memory_percentages:
         script_file.write("node_names = [\"worker1\", \"worker2\", \"worker3\"]\n")
         script_file.write(f"loop_times = {loop_times}\n\n")
         script_file.write(f"program_name = \"{program_name}\"\n")
-        script_file.write(f"app_name = \"{app_name}\"\n\n")
+        script_file.write(f"app_name1 = \"{app_name1}\"\n")
+        script_file.write(f"app_name1 = \"{app_name2}\"\n\n")
         
         script_file.write("for i in range(len(node_names)*loop_times):\n")
         script_file.write("  node_name = node_names[i % len(node_names)]\n")
-        script_file.write(f"  job_manifest = job_template.format(index=i, memory_request={memory_request}, memory_limit={memory_limit}, program_name=program_name, app_name=app_name)\n")
+        script_file.write(f"  job_manifest = job_template.format(index=i, memory_request={memory_request}, memory_limit={memory_limit}, program_name=program_name, app_name1=app_name1, app_name2=app_name2)\n")
         script_file.write("  with open(f'job-{i}.yaml', 'w') as f:\n")
         script_file.write("    f.write(job_manifest)\n")
         script_file.write("  os.system(f'kubectl apply -f job-{i}.yaml')\n")
--- splash2x-ocean_cp/splash2x-ocean_cp-140%.py
+++ splash2x-ocean_cp/splash2x-ocean_cp-140%.py
@@ -5,17 +5,17 @@ job_template = '''
 apiVersion: v1
 kind: Pod
 metadata:
-  name: {program_name}-{app_name}{index}
+  name: {program_name}-{app_name2}{index}
   labels:
-    app: {app_name}{index}
+    app: {app_name2}{index}
 spec:
   restartPolicy: OnFailure
   containers:
-  - name: {app_name}{index}
+  - name: {app_name2}{index}
     image: docker.io/limitstory/parsec-modify:latest
     imagePullPolicy: IfNotPresent
     command: ["/home/parsec-3.0/run"]
-    args: ["-a", "run", "-p", "{program_name}.{app_name}", "-i", "native"]
+    args: ["-a", "run", "-p", "{program_name}.{app_name1}", "-i", "native"]
     resources:
       requests:
         memory: {memory_request}Mi
@@ -28,11 +28,12 @@ node_names = ["worker1", "worker2", "worker3"]
 loop_times = 20
 
 program_name = "splash2x"
-app_name = "ocean_cp"
+app_name1 = "ocean_cp"
+app_name1 = "ocean-cp"
 
 for i in range(len(node_names)*loop_times):
   node_name = node_names[i % len(node_names)]
-  job_manifest = job_template.format(index=i, memory_request=2750, memory_limit=3850, program_name=program_name, app_name=app_name)
+  job_manifest = job_template.format(index=i, memory_request=2750, memory_limit=3850, program_name=program_name, app_name1=app_name1, app_name2=app_name2)
   with open(f'job-{i}.yaml', 'w') as f:
     f.write(job_manifest)
   os.system(f'kubectl apply -f job-{i}.yaml')
